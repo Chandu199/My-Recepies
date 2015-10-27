6ebcd02cd9b1d00756cef11fb6fb7c85c7492fd4
chandrakanth1993 <n.chandrakanth0@gmail.com>
2015-10-27 21:44:55 +0000

    added like and dislike

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController <ApplicationController
     def index
-        @recipes=Recipe.all
+        @recipes=Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
         
     end
     def show
@@ -40,7 +40,17 @@ class RecipesController <ApplicationController
         end
     end
     
-    
+    def like 
+        @recipe=Recipe.find(params[:id])
+        like=Like.create(like:params[:like],chef: Chef.first, recipe: @recipe)
+        if like.valid?
+            flash[:success]="selection was successfull"
+            redirect_to :back
+        else
+            flash[:danger]="not added as already selection made"
+            redirect_to :back
+        end
+    end
     
     private
     
